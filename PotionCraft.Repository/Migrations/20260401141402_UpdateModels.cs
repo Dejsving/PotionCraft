0f85d9183dca36a29b803eac5fe47b34a4564f56
Артур Хисматуллин <OtusBasicGradWork>
2026-04-01 17:16:19 +0300

    Правки миграций

---
+++ PotionCraft.Repository/Migrations/20260401141402_UpdateModels.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PotionCraft.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Bag_Coins",
+                table: "PlayerCharacters",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bag_Coins",
+                table: "PlayerCharacters");
+        }
+    }
+}
