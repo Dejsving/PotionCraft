0a1904ae0726ad5f681248844a44de4859bfa3be
Dejsving <Dejsving@YRBANUSLAPTOP>
2026-07-14 02:10:49 +0300

    Исправлена миграция

---
+++ PotionCraft.Repository/Migrations/20260713230929_SyncModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PotionCraft.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifierEffect",
+                table: "Herbs",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldDefaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifierEffect",
+                table: "Herbs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
